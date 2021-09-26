2a01c08e3a65de3b3920a8ef118f7451baae242d
Jennifer Lim <jennifer.lim@redfin.com>
2021-09-26 11:49:07 -0700

    passing top k frequent elements

--- hash_practice/exercises.py
+++ hash_practice/exercises.py
@@ -2,29 +2,45 @@
 def grouped_anagrams(strings):
     """ This method will return an array of arrays.
         Each subarray will have strings which are anagrams of each other
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(n)
     """
     hash_table = {}
     for word in strings:
-        # print(word)
         sort_word = ''.join(sorted(word))
-        print(sort_word)
         if sort_word in hash_table:
             hash_table[sort_word].append(word)
         else:
             hash_table[sort_word] = [word]
-    print(hash_table)
-    print(list(hash_table.values()))
+
     return list(hash_table.values())
 
 def top_k_frequent_elements(nums, k):
     """ This method will return the k most common elements
         In the case of a tie it will select the first occuring element.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(n)
     """
-    pass
+    if not nums:
+        return []
+
+    hash_table = {}
+    for num in nums:
+        if num in hash_table:
+            hash_table[num] += 1
+        else:
+            hash_table[num] = 1
+
+    print(hash_table)
+    sort_values = sorted(hash_table, key=lambda x: hash_table[x], reverse=True)
+    print(sort_values)
+
+    elements = []
+    for i in range(0, k):
+        elements.append(sort_values[i])
+
+    print(elements)
+    return elements
 
 
 def valid_sudoku(table):
