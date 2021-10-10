b1914452fdeaf1b560aceebda38ee602b809f5fa
Sneha <sneha8412@gmail.com>
2021-10-09 20:23:34 -0700

    exercise 1 & 2 tests pass

--- hash_practice/exercises.py
+++ hash_practice/exercises.py
@@ -5,7 +5,24 @@ def grouped_anagrams(strings):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    anagram_dict = {}
+
+    for word in strings:
+        # Hash Formula maps anagrams into same bucket
+        # by alphabetically sorting the word
+        # Alpha sort ensures words with same length and same freq
+        # of characters get mapped into same bucket
+        word_key = "".join(sorted(word))
+        if (word_key in anagram_dict):
+            anagram_dict[word_key].append(word)
+        else:
+            anagram_dict[word_key] = [word]
+
+    output = []
+    for word_key in anagram_dict:
+        output.append(anagram_dict[word_key])
+
+    return output
 
 def top_k_frequent_elements(nums, k):
     """ This method will return the k most common elements
@@ -13,7 +30,46 @@ def top_k_frequent_elements(nums, k):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    num_dict = {}
+    result = []
+
+    freq_dict = {}
+    num_freqs = []
+
+    #polulate the dictionary
+    for i in nums:
+        #map key to value
+        if i not in num_dict:
+            num_dict[i] = 1
+        else:
+            num_dict[i] += 1
+
+    
+    for key,value in num_dict.items():
+        if (value not in freq_dict):
+            freq_dict[value] = [key]
+        else:
+            freq_dict[value].append(key)
+
+        # num_freqs is list of unique frequencies
+        if (value not in num_freqs):
+            num_freqs.append(value)
+        
+    num_freqs.sort(reverse=True)
+
+    for freq in num_freqs:
+        nums_for_freq = freq_dict[freq]
+        for num in nums_for_freq:
+            if(len(result) < k):
+                result.append(num)
+            else:
+                break
+        
+    return result
+
+    
+
+
 
 
 def valid_sudoku(table):
@@ -27,3 +83,12 @@ def valid_sudoku(table):
     """
     pass
 
+
+# numbers = [1, 2, 3]
+# k = 3
+# output = top_k_frequent_elements(numbers, k)
+# print(output)
+
+#input = [] #["eat", "tea", "tan", "ate", "nat", "bat"]
+#output = grouped_anagrams(input)
+#print(output)
