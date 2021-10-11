95e816891090ee27bec80c0844f027087ae2eeee
Sneha Agarwal <68921168+sneha8412@users.noreply.github.com>
2021-10-10 19:05:03 -0700

    sodoku tests pass

--- hash_practice/exercises.py
+++ hash_practice/exercises.py
@@ -78,8 +78,60 @@ def valid_sudoku(table):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    
+    row_counts = []
+    col_counts = []
+    subgrid_counts = [] # hash of hash where each sub hash represents a 3x3 grid
+    
+    row_index = 0
+    
+    for row in table:
+        col_index = 0
+        #initialize the hashtable for a specific row
+        row_counts.append({})
+        
+        for element in row:
+            
+            # initialize the hashtable for a specific col if it doesnt exist
+            if (len(col_counts) <= col_index):
+                col_counts.append({})
+            
+            if (element != "."):
+                if(not update_and_validate_hash(element, row_counts[row_index])):
+                    return False
+                
+                if(not update_and_validate_hash(element, col_counts[col_index])):
+                    return False    
+            
+            col_index += 1        
+        
+        row_index += 1
+        
+    return True           
 
+# helper function to add element to hashtable for a specific row or col and validate counts    
+def update_and_validate_hash(element, hashtable):
+    if(element not in hashtable):
+        hashtable[element] = 1
+    else:
+        hashtable[element] += 1
+    
+    return (hashtable[element] <= 1)
+
+
+# table = [
+#   ["5","3",".",".","7",".",".",".","."],
+#   ["6",".",".","1","9","5",".",".","."],
+#   [".","9","8",".",".",".",".","6","."],
+#   ["8",".",".",".","6",".",".",".","3"],
+#   ["4",".",".","8",".","3",".",".","1"],
+#   ["7",".",".",".","2",".",".",".","6"],
+#   [".","6",".",".",".",".","2","8","."],
+#   [".",".",".","4","1","9",".",".","5"],
+#   [".",".",".",".","8",".",".","7","9"]
+# ]
+# output = valid_sudoku(table)
+# print(f"output={output}")
 
 # numbers = [1, 2, 3]
 # k = 3
