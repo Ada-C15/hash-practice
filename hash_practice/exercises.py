a250cb7ee315de07fab84a874205111491ecf969
Nataliia Getlin <natali.getlin@gmail.com>
2021-10-18 08:52:21 -0700

    Complete and test grouped_anagrams method

--- hash_practice/exercises.py
+++ hash_practice/exercises.py
@@ -5,7 +5,60 @@ def grouped_anagrams(strings):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    prime_numbers = [
+        3,
+        5,
+        7,
+        11,
+        13,
+        17,
+        19,
+        23,
+        29,
+        31,
+        37,
+        41,
+        43,
+        47,
+        53,
+        59,
+        61,
+        67,
+        71,
+        73,
+        79,
+        83,
+        89,
+        97,
+        101,
+        103]
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    if len(strings) == 0:
+        return []
+
+    def construct_mapping_letter_to_number(letters, numbers):
+        map = {}
+        for index in range(len(letters)):
+            map[letters[index]] = numbers[index]
+        return map
+
+    letter_to_prime_mapping = construct_mapping_letter_to_number(
+        alphabet, prime_numbers)
+    hash_table = {}
+
+    for string in strings:
+        hash_index = 1
+        for character in string:
+            hash_index = hash_index * letter_to_prime_mapping[character]
+
+        if not hash_table.get(hash_index):
+            hash_table[hash_index] = [string]
+        else:
+            hash_table[hash_index].append(string)
+
+    return list(hash_table.values())
+
 
 def top_k_frequent_elements(nums, k):
     """ This method will return the k most common elements
@@ -20,10 +73,12 @@ def valid_sudoku(table):
     """ This method will return the true if the table is still
         a valid sudoku table.
         Each element can either be a ".", or a digit 1-9
-        The same digit cannot appear twice or more in the same 
+        The same digit cannot appear twice or more in the same
         row, column or 3x3 subgrid
         Time Complexity: ?
         Space Complexity: ?
     """
     pass
 
+
+grouped_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
