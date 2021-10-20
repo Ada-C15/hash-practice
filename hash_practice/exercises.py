da26145795fa459760025ce8722f4bf3000be3b2
Ari <ari.go.org.7@gmail.com>
2021-10-19 19:18:53 -0700

    fixed comments

--- hash_practice/exercises.py
+++ hash_practice/exercises.py
@@ -2,19 +2,13 @@
 def grouped_anagrams(words):
     """ This method will return an array of arrays.
         Each subarray will have strings which are anagrams of each other
-        Time Complexity: ? Sorting (O log n) * n log n
+        Time Complexity: O(m n log n)
         Space Complexity: O(n)
     """
     anagrams = []
     str_map = {}
     for word in words:
-        # sorted_word = word # initial string
-        # sorted_arr = sorted(word)
-        # sorted_word.join(sorted_arr)
-
         sorted_word = ''.join(sorted(word))
-        print("------- sorted_word----------- ")
-        print(sorted_word)
         if sorted_word not in str_map:
             str_map[sorted_word] = [word]
         elif sorted_word in str_map:
@@ -27,12 +21,12 @@ def grouped_anagrams(words):
 def top_k_frequent_elements(nums, k):
     """ This method will return the k most common elements
         In the case of a tie it will select the first occuring element.
-        Time Complexity: O(kn)
+        Time Complexity: O(k n)
         Space Complexity: O(k)
     """
     if not nums:
         return []
-        
+
     num_dict = {} 
     for num in nums:
         if num not in num_dict:
