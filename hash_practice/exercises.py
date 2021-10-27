9114caf978053c535d9e99d1206019d862a9d111
melissa-nguyen <n.melissa.25@gmail.com>
2021-10-27 13:57:48 -0700

    passing k frequent element tests

--- hash_practice/exercises.py
+++ hash_practice/exercises.py
@@ -13,7 +13,43 @@ def top_k_frequent_elements(nums, k):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+
+    #  return empty list if nums is empty
+    if not nums:
+        return []
+
+    # create an empty hash table. key = number, value = occurence in string 
+    # iterate through nums 
+    # if number is not currently in the table, add it to the table and update value to 1
+    # if number is in table, update value
+    nums_table = {}
+    for number in nums:
+        if number in nums_table:
+            nums_table[number] += 1
+        else:
+            nums_table[number] = 1
+    
+
+    # create an empty array 
+    # iterate through hash table and append key to array based on value - in desc order
+    # delete key from original hash table so we don't see it again
+    nums_arr = []
+
+    # loop through K # of times
+    for n in range(k):
+        max_number = 0
+        frequency = 0
+
+        for num, count in nums_table.items():
+            if count > frequency:
+                frequency = count
+                max_number = num
+        
+        nums_arr.append(max_number)
+        del nums_table[max_number]
+    
+    return nums_arr
+    
 
 
 def valid_sudoku(table):
