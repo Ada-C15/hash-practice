5f6715f92720112fff6d0dfc18125bbae8057d6e
maite <arp.maite@gmail.com>
2021-11-02 18:07:49 -0700

    adds solutions to hash practice

--- hash_practice/exercises.py
+++ hash_practice/exercises.py
@@ -2,19 +2,48 @@
 def grouped_anagrams(strings):
     """ This method will return an array of arrays.
         Each subarray will have strings which are anagrams of each other
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(n)
     """
-    pass
+    hash = {}
+    
+    for string in strings: 
+        sorted_word = "".join(sorted(string))
+        if sorted_word in hash:
+            hash[sorted_word].append(string)
+        else:    
+            hash[sorted_word] = [string]
+    
+    return list(hash.values())
+
+
+def get_tuple_key(item):
+    return item[1]
 
 def top_k_frequent_elements(nums, k):
     """ This method will return the k most common elements
         In the case of a tie it will select the first occuring element.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n log n)
+        Space Complexity: O(n) 
     """
-    pass
-
+    my_hash = {}
+    
+    if len(nums) == 0: 
+        return []
+    
+    for num in nums:
+        if num in my_hash:
+            my_hash[num] = my_hash.get(num, 1) + 1
+        else:
+            my_hash[num] = 1
+    answer = []
+    temp = sorted(my_hash.items(), key=get_tuple_key, reverse=True)# helper function above
+    for key, value in temp:
+        answer.append(key)
+    return answer[0:k]
+    
+        
+        
 
 def valid_sudoku(table):
     """ This method will return the true if the table is still
