e728059baa2b9e39873a715223a3cac5a781c51a
ggrossvi <hello.gloria@gmail.com>
2021-11-29 20:45:05 -0800

    added grouped anagrams

--- hash_practice/exercises.py
+++ hash_practice/exercises.py
@@ -1,11 +1,43 @@
 
+
+
 def grouped_anagrams(strings):
     """ This method will return an array of arrays.
         Each subarray will have strings which are anagrams of each other
         Time Complexity: ?
         Space Complexity: ?
-    """
-    pass
+    """ 
+
+    
+    anagrams = {}
+    if strings == []:
+        return []
+    for word in strings:
+        hash = 0
+        for letter in word:
+            unicode_char = ord(letter)
+            hash += unicode_char
+        if hash in anagrams:
+            anagrams[hash].append(word)
+        else:
+            anagrams[hash] = [word]
+
+
+    return create_anagram_list(anagrams)
+
+def create_anagram_list(anagrams):
+    grouped_anagrams = []
+    for key, value in anagrams.items():
+        grouped_anagrams.append(value)
+    return grouped_anagrams
+
+    # split each word 
+    # get ascii for each letter
+    # add letters/ascii values together for each word
+    # make that the key and add word to anagram/value list 
+    # return anagram list for each key  
+
+    
 
 def top_k_frequent_elements(nums, k):
     """ This method will return the k most common elements
