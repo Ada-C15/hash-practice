1205560b5687e52f9806e583c8873e0485c86857
Andrea Palacios <andreapalacios88@gmail.com>
2022-01-23 22:39:03 -0800

    anagram and top_k tests passing

--- hash_practice/exercises.py
+++ hash_practice/exercises.py
@@ -2,18 +2,47 @@
 def grouped_anagrams(strings):
     """ This method will return an array of arrays.
         Each subarray will have strings which are anagrams of each other
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: 0(n)
+        Space Complexity: O(n)
     """
-    pass
+    if not strings:
+        return []
 
-def top_k_frequent_elements(nums, k):
+    map={}
+    for string in strings:
+        sorted_string = ''.join(sorted(string))
+        if sorted_string in map:
+            map[sorted_string].append(string)
+        else:
+            map[sorted_string]=[string]
+
+    result=[]
+    for value in map.values():
+        result.append(value)
+    return result
+
+
+def top_k_frequent_elements(nums,k):
     """ This method will return the k most common elements
         In the case of a tie it will select the first occuring element.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n log n)
+        Space Complexity: O(n)
     """
-    pass
+    if not nums:
+        return []
+
+    map = {}
+    for num in nums:
+        count = map.get(num,0)
+        map[num]= count +1
+    
+    result= []
+    for i in range(k):
+        k = max(map, key=map.get)
+        result.append(k)
+        del map[k]
+
+    return result
 
 
 def valid_sudoku(table):
