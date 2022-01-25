c92fc0ad05ad401495ed01664fca704c1a386dcd
brijhaywood <briyanahaywood@gmail.com>
2022-01-24 23:10:17 -0600

    Hash tables completed

--- hash_practice/exercises.py
+++ hash_practice/exercises.py
@@ -2,20 +2,40 @@
 def grouped_anagrams(strings):
     """ This method will return an array of arrays.
         Each subarray will have strings which are anagrams of each other
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(n) 
     """
-    pass
+    anagrams = {}
+    for word in strings:
+        sorted_word = "".join(sorted(word))
+        if sorted_word in anagrams:
+            anagrams[sorted_word].append(word)
+        else:
+            anagrams[sorted_word] = [word]
+    return list(anagrams.values())
 
 def top_k_frequent_elements(nums, k):
     """ This method will return the k most common elements
         In the case of a tie it will select the first occuring element.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(n)
     """
-    pass
+    if nums == []:
+        return []  
+    frequency = {}
+    sorted_k = [] 
+    for num in nums:
+        if num in frequency:
+            frequency[num] += 1
+        else:
+            frequency[num] = 1
+    for i in range(k): 
+        highest_frequency = max(frequency,key=frequency.get)
+        sorted_k.append(highest_frequency)
+        frequency.pop(highest_frequency)
+    return sorted_k
 
-
+# OPTIONAL   
 def valid_sudoku(table):
     """ This method will return the true if the table is still
         a valid sudoku table.
@@ -25,5 +45,6 @@ def valid_sudoku(table):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    return "valid"
+
 
