9f36bf925d168ca3d4e6cbd8717c412c77d95642
Erika Chikere <erikachik08@gmail.com>
2022-01-27 06:00:56 -0800

    Passes all tests

--- hash_practice/exercises.py
+++ hash_practice/exercises.py
@@ -2,18 +2,42 @@
 def grouped_anagrams(strings):
     """ This method will return an array of arrays.
         Each subarray will have strings which are anagrams of each other
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n log n)
+        Space Complexity: O(n)
     """
-    pass
+    anagrams = {}
+    for word in strings:
+        sorted_word = "".join(sorted(word))
+        if sorted_word in anagrams:
+            anagrams[sorted_word].append(word)
+        else:
+            anagrams[sorted_word] = [word]
+    return list(anagrams.values())
 
 def top_k_frequent_elements(nums, k):
     """ This method will return the k most common elements
         In the case of a tie it will select the first occuring element.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n log n)
+        Space Complexity: O(n)
     """
-    pass
+    temp_dict = {}
+    return_list = []
+
+    if not nums:
+        return return_list
+
+    for i in range(0, len(nums)):
+        if nums[i] in temp_dict:
+            temp_dict[nums[i]] += 1
+        else:
+            temp_dict[nums[i]] = 1
+
+    sorted_dict = sorted(temp_dict.items(), key=lambda tuple: tuple[1], reverse=True)
+
+    for j in range(0, k):
+        return_list.append(sorted_dict[j][0])
+
+    return return_list
 
 
 def valid_sudoku(table):
@@ -22,8 +46,8 @@ def valid_sudoku(table):
         Each element can either be a ".", or a digit 1-9
         The same digit cannot appear twice or more in the same 
         row, column or 3x3 subgrid
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(1)
+        Space Complexity: O(1)
+        Will provide solution with the above time/space complexity. 
     """
-    pass
-
+    pass 
